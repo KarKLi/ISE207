bae4b863bc1a1c936489959d91d7b4a2a44afea2
KarK Li <34907015+KarKLi@users.noreply.github.com>
2018-10-23 18:01:20 +0800

    Add FFT and sample code

--- Fourier Transform/Fourier Transform/main.cpp
+++ Fourier Transform/Fourier Transform/main.cpp
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 #include "FT.h"
 #include <opencv2\opencv.hpp>
 #include <opencv2\highgui.hpp>
 using namespace cv;
 int main()
 {
-	Mat src = imread("C:\\Users\\KarK\\Desktop\\image\\peppers.bmp", 1);
+	Mat src = imread("C:\\Users\\KarK\\Desktop\\image\\changfangxing.bmp", 1);
 	if (src.empty())
 		exit(1);
 	if (src.type() != CV_8U)
@@ -30,7 +29,7 @@ int main()
 		uchar *p = dst.ptr<uchar>(i);
 		for (int j = 0; j < src.cols; j++)
 		{
-			int tmp = round(150.0*(255.0*log2(1.0 + Dst[i][j]/255)));
+			int tmp = round(100.0*(255.0*log2(1.0 + Dst[i][j] / 255)));
 			if (tmp > 255)
 				tmp = 255;
 			p[j] = (uchar)tmp;
@@ -49,7 +48,7 @@ int main()
 		uchar *p = dst.ptr<uchar>(i);
 		for (int j = 0; j < src.cols; j++)
 		{
-			int tmp = round(150.0*(255.0*log2(1.0 + Dst[i][j] / 255)));
+			int tmp = round(100.0*(255.0*log2(1.0 + Dst[i][j] / 255)));
 			if (tmp > 255)
 				tmp = 255;
 			p[j] = (uchar)tmp;
@@ -78,52 +77,4 @@ int main()
 	}
 	imwrite("C:\\Users\\KarK\\Desktop\\image\\IDFT.bmp", dst);
 	return 0;
-=======
-#include "FT.h"
-#include <opencv2\opencv.hpp>
-#include <opencv2\highgui.hpp>
-using namespace cv;
-int main()
-{
-	Mat src = imread("C:\\Users\\Admin\\Desktop\\lena.bmp", 0);
-	if (src.empty())
-		exit(1);
-	vector<vector<FT::Complex>>Image;
-	Image.resize(src.rows);
-	for (uint i = 0; i < src.rows; i++)
-		Image[i].resize(src.cols);
-	for (uint i = 0; i < src.rows; ++i)
-		for (uint j = 0; j < src.cols; ++j)
-			Image[i][j] = (double)src.at<uchar>(i, j);
-	FT::DFTTwoDim dfts(src.rows, src.cols);
-	dfts.LoadData(Image);
-	dfts.DFT(false);
-	Mat dst;
-	src.copyTo(dst);
-	vector<vector<double>>Dst(src.rows, vector<double>(src.cols));
-	vector<vector<FT::Complex>>Dstcomp(src.rows, vector<FT::Complex>(src.cols));
-	dfts.GetAmplitudeArray(Dst, src.rows, src.cols);
-	for (int i = 0; i < src.rows; i++)
-	{
-		uchar *p = dst.ptr<uchar>(i);
-		for (int j = 0; j < src.cols; j++)
-		{
-			int tmp=round(150.0*(255.0*log2(1.0 + Dst[i][j] / 255)));
-			if (tmp > 255)
-				tmp = 255;
-			p[j] = (uchar)tmp;
-		}
-	}
-	imwrite("C:\\Users\\Admin\\Desktop\\DFTAmplitude.bmp",dst);
-	dfts.InverseDFT();
-	dfts.WriteData(Dstcomp);
-	for (int i = 0; i < src.rows; i++)
-	{
-		uchar *p = dst.ptr<uchar>(i);
-		for (int j = 0; j < src.cols; j++)
-			p[j] = (uchar)Dstcomp[i][j].real;
-	}
-	imwrite("C:\\Users\\Admin\\Desktop\\IDFT.bmp", dst);
-  	return 0;
->>>>>>> ac8337cffc1b28815bc76664cd8dc49c62a1b627
 }
